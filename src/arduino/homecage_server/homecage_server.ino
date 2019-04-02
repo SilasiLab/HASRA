7d6438a718f9dd528e6761a462982e9a9b5a74c3
Frank <alchemistWu0521@gmail.com>
2019-04-02 11:14:34 -0400

    fixed the bug in arduino.

--- src/arduino/homecage_server/homecage_server.ino
+++ src/arduino/homecage_server/homecage_server.ino
@@ -383,6 +383,7 @@ int startSession() {
             default:
               break;  
             }
+          break;
         case ('4'):
           displayPellet(left);
           displayPellet(right);
